f3ab04d8fda93401535995fde126a87963d94da3
Alí Guzmán Adán <aliguzmanadan@MacBook-Pro.local>
2021-11-08 16:22:20 +0100

    Models Category and Auction_listing

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Auction_listing, Category
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Auction_listing)
+admin.site.register(Category)
--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.fields import BooleanField, CharField, FloatField, TextField, URLField
+from django.db.models.fields.related import ForeignKey, ManyToManyField
 
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    title = CharField(max_length=64)
+
+class Auction_listing(models.Model):
+    title = CharField(max_length=64)
+    initial_price = FloatField()
+    description = TextField(blank=True)
+    creator = ForeignKey(User, on_delete=models.CASCADE, related_name="created_listings")
+    winner = ForeignKey(User, on_delete=models.CASCADE, related_name="won_listings", blank=True)
+    is_active = BooleanField(default=True)
+    image_link = URLField(max_length=500, blank=True)
+    categories = ManyToManyField(Category, blank=True, related_name="listings_per_category")
+    in_watch_list = ManyToManyField(User, blank=True, related_name="wishes_per_user")
