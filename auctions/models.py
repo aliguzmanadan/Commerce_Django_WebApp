36660f137711a2962b8c4111b90d2fbfdd15ec43
Alí Guzmán Adán <aliguzmanadan@MacBook-Pro.local>
2021-11-08 17:13:36 +0100

    Adding Listings in idex page (no links)

--- auctions/models.py
+++ auctions/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.deletion import CASCADE
-from django.db.models.fields import BooleanField, CharField, FloatField, TextField, URLField
+from django.db.models.fields import BooleanField, CharField, DecimalField, FloatField, TextField, URLField
 from django.db.models.fields.related import ForeignKey, ManyToManyField
 
 
@@ -13,14 +13,17 @@ class Category(models.Model):
 
 class Auction_listing(models.Model):
     title = CharField(max_length=64)
-    initial_price = FloatField()
+    initial_price = DecimalField(max_digits=10, decimal_places=2)
     description = TextField(blank=True)
     creator = ForeignKey(User, on_delete=models.CASCADE, related_name="created_listings")
-    winner = ForeignKey(User, on_delete=models.CASCADE, related_name="won_listings", blank=True)
+    winner = ForeignKey(User, on_delete=models.CASCADE, related_name="won_listings", blank=True, null=True)
     is_active = BooleanField(default=True)
     image_link = URLField(max_length=500, blank=True)
     categories = ManyToManyField(Category, blank=True, related_name="listings_per_category")
     in_watch_list = ManyToManyField(User, blank=True, related_name="wishes_per_user")
+
+    def __str__(self):
+        return f"{self.title}, initial price: {self.initial_price}, creator: {self.creator} "
 
 class Bid(models.Model):
     amount = FloatField()
